f214985baf99f5fb1260c3759a343761cc1b094c
Hongbin Li <hblee12294@gmail.com>
2020-03-17 23:15:31 -0700

    build: clean repo

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static(__dirname))
-
-// router
-app.get('/', (req, res) => res.send('Hi'))
-
-app.listen(5000, () => console.log('Test app listening on http://localhost:5000 ...'))
