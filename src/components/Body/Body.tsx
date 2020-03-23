efa281bf28885e5ba7cdeadd493fde3b4518365b
Hongbin Li <hblee12294@gmail.com>
2020-03-22 23:56:56 -0700

    feat: add works and animations

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -7,10 +7,10 @@ const Body: React.FC = () => {
   return (
     <div className="body">
       <header>
-        <h1 className="title">HBLEE</h1>
+        <h1 className="title">HONGBIN LI</h1>
         <p className="intro">
-          An enthusiastic web developer / front-end engineer. Put an eye on web development technology, design, finance,
-          etc.
+          An enthusiastic web developer / front-end engineer. Put an eye on web development technology, design, fasion,
+          finance, world, etc.
         </p>
         <ul className="links">
           <li className="links-item">
@@ -26,6 +26,28 @@ const Body: React.FC = () => {
           <li className="links-item">
             <a href="https://www.instagram.com/hblee12294/" target="_blank" rel="noopener noreferrer">
               <Icon type="Instagram" />
+            </a>
+          </li>
+        </ul>
+        <ul className="works">
+          <li className="works-item">
+            <a
+              className="works-link"
+              href="https://hongbinli.com/three-archive"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              THREE ARCHIVE
+            </a>
+          </li>
+          <li className="works-item">
+            <a
+              className="works-link"
+              href="https://hongbinli.com/quantitative-life"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              QUANTITATIVE LIFE
             </a>
           </li>
         </ul>
